e42b7fac31104137927b01b248e653c0807b3ae1
Chetan Rao Sonoo <23048457@imail.sunway.edu.my>
2026-09-01 18:31:25 +0800

    chore: add quality gates and ci

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+
+const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    ignores: [
+      ".next/**",
+      "next-env.d.ts",
+      "node_modules/**",
+      "out/**",
+      "coverage/**",
+      "playwright-report/**",
+      "test-results/**",
+    ],
+  },
+];
+
+export default eslintConfig;
